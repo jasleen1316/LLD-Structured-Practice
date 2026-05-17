d9cab88786cf89f610025d312d6b6001f5ecfd72
jasleen1316 <jasleenkaur.aneja@walmart.com>
2026-05-17 16:38:48 +0530

    setup

---
+++ src/main/java/com/jasleen/lld/App.java
@@ -0,0 +1,20 @@
+package com.jasleen.lld;
+
+import com.jasleen.lld.beginner.Logger;
+import com.jasleen.lld.beginner.ShapeAreaCalculator;
+import com.jasleen.lld.beginner.TextDecorator.BoldTextDecorator;
+import com.jasleen.lld.beginner.TextDecorator.ItalicTextDecorator;
+import com.jasleen.lld.beginner.TextDecorator.SimpleText;
+import com.jasleen.lld.beginner.TextDecorator.TextDecorator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+    }
+}
